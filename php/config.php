41f1a13ca5d68a7983b0f97deaf9405b36facc63
ZalvadorK <martysalva17@gmail.com>
2025-08-11 21:31:26 -0600

    config.php

--- php/config.php
+++ php/config.php
@@ -1,10 +1,10 @@
 <?php
 // Datos para conectar con la base de datos MySQL
-$servername = mysql.railway.internal; // Servidor
-$username = root; // Usuario de MySQL
-$password = xBcXIMlCrrQWqDgyYijzeSIXuvhSbTHM; // Contraseña de MySQL
-$dbname = railway; //Nombre de la base de datos
-$puerto = 3306; //Puerto MySQL
+$servername = getenv("DB_HOST"); // Servidor
+$username = getenv("DB_USERNAME"); // Usuario de MySQL
+$password = getenv("DB_PASSWORD"); // Contraseña de MySQL
+$dbname = getenv("DB_DATABASE"); //Nombre de la base de datos
+$puerto = getenv("DB_PORT"); //Puerto MySQL
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $$dbname, $puerto);
